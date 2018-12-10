015bfd709dab5d068a4b2546fb776f61c2f7223c
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2018-12-09 19:18:59 -0600

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+
+	public String name;
+	public int Intelligence;
+
+	public Person(){
+		name = "Default";
+		Intelligence = 100;
+	}
+
+	public Person(String n, int smarts) {
+		name = n;
+		Intelligence = smarts;
+	}
+
+
+}
